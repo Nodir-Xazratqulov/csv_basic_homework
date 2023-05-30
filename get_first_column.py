ac14426096563b1aae77fb6be530f4a5e4cb9ff7
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-05-30 22:02:00 +0500

    csv04

--- get_first_column.py
+++ get_first_column.py
@@ -6,6 +6,14 @@ def get_first_column(data):
     Return:
         list: First column.
     """
-    return
-    
+    f=open(data, mode='r')
+    r=f.read()
+    list1=[]
+    list2=[]
+    for i in r.split('\n'):
+        list1.append(i.split(','))
+    for k in list1:
+        list2.append(k[0])
+    return list2
+print(get_first_column('data.csv'))
 # Read the csv file
