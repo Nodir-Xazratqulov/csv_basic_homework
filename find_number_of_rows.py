3db8ca0e56ba38b55c910e294ee83f4763129668
Nodir-Xazratqulov <nodirxazratqulov2110@gmail.com>
2023-05-30 22:00:09 +0500

    csv02

--- find_number_of_rows.py
+++ find_number_of_rows.py
@@ -6,6 +6,17 @@ def find_number_of_rows(data):
     Return:
         int: Number of rows.
     """
-    return
+    
+    f=open(data, mode='r')
+    r=f.read()
+    list1 = []
+    for i in r.split('\n'):
+        list1.append(i)
+    k=0
+    for j in list1:
+        k+=1
+    return k
+
+print(find_number_of_rows('data.csv'))
 
 # Read the csv file
